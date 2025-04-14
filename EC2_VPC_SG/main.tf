f62cef3f2aad6bdcf51670918ef864933409e8b6
JhenkarG <129648894+JhenkarG@users.noreply.github.com>
2025-04-14 08:35:18 +0530

    Update main.tf

--- EC2_VPC_SG/main.tf
+++ EC2_VPC_SG/main.tf
@@ -2,4 +2,86 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+  
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "Public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-talbe"
+  }
+  
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.Public_rt.id
+}
+
+resource "aws_security_group" "ssh_sg" {
+  name = "Allow_ssh"
+  description = "Allow SHH as inbound traffic"
+  vpc_id = aws_vpc.main.id
+  
+
+ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-sg"
+  }
+}
+resource "aws_instance" "example" {
+  ami                    = "ami-084568db4383264d4"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+  key_name               = "Jenkin-Ubuntu-Keypair"
+
+  tags = {
+    Name = "Demo EC2 with SSH"
+  }
+}
